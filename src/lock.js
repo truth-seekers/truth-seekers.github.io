a64f2e7ac271ef84597dd03114d0e9aa52953b54
Nic MacBeth <nicolasmacbeth@gmail.com>
2019-03-26 18:24:54 -0400

    final

---
+++ src/lock.js
@@ -0,0 +1,3 @@
+do {
+    input = prompt("Sorry! Our website is unavailable until March 30th at noon when our first podcast drops!");
+} while (input == "" && input == null && input!="gangganggang");
